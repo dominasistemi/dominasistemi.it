059b96682f734d491501e06980d95319f8e8fe5c
Alessandro Desantis <desa.alessandro@gmail.com>
2019-04-13 16:28:55 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,5 @@
+activate :external_pipeline,
+  name: :webpack,
+  command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
+  source: "tmp/webpack-dist",
+  latency: 1
